c4d81ba2e72d05b8be3a4ac97c21816671360170
xw.hope <xw.hope@qq.com>
2021-10-28 16:23:45 +0800

    add some comments

--- print.py
+++ print.py
@@ -4,4 +4,6 @@ print("we use git now")
 
 print("we use git-hub now")
 
-print("we want push our code to git-hub now")
+#print("we want push our code to git-hub now")
+
+print("we try to push git-hub now")
